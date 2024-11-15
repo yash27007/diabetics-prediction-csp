fc639a09bd02df78cb94b7d76bbea54d6c53ddb3
yash27007 <work.yashwantharavind007@gmail.com>
2024-11-15 22:49:51 +0530

    final commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import axios  from "axios";
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.post('/predict', async (req, res) => {
   try {
